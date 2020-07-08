c2c784576be1c7ffc85b14b5f6fc055dbfa9a910
Marina8989 <marina.mill209@gmail.com>
2020-07-07 19:17:10 -0700

    added new book, now need to display it on the creen

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ class UI {
                 isbn: 8668
             }
         ];
-        const books = StoreBooks;
+        const books = StoredBooks;
 
         books.forEach(book => UI.addBookToList(book))
     }
@@ -29,22 +29,34 @@ class UI {
     static addBookToList(book) {
         const list = document.querySelector('#book-list');
 
-        const li = document.createElement('li');
+        const row = document.createElement('tr');
 
-        li.innerHTML = `
+        row.innerHTML = `
           <td>${book.title}</td>
           <td>${book.author}</td>
           <td>${book.isbn}</td>
           <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
         `;
 
-        list.appendChild(li);
+        list.appendChild(row);
     }
 }
 //Store class: handles storage (local)
 
 //Event: Display Books
-
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 //Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //instantiate book 
+    const book = new Book(title, author, isbn);
+
+})
 //Event: Remove a Book
  
