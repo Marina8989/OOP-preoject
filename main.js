8eeff8e339d5499c5e8c04ec6c6e2b0ef26114db
Marina8989 <marina.mill209@gmail.com>
2020-07-07 20:20:17 -0700

    half way done

--- main.js
+++ main.js
@@ -22,13 +22,11 @@ class UI {
             }
         ];
         const books = StoredBooks;
-
         books.forEach(book => UI.addBookToList(book))
     }
 
     static addBookToList(book) {
         const list = document.querySelector('#book-list');
-
         const row = document.createElement('tr');
 
         row.innerHTML = `
@@ -39,6 +37,17 @@ class UI {
         `;
 
         list.appendChild(row);
+    }
+    static deleteBook(el) {
+        if(el.classList.contains('delete')) {
+           el.parentElement.parentElement.remove();
+        }
+    } 
+
+    static clearFields() {
+        const title = document.querySelector('#title').value = '';
+        const author = document.querySelector('#author').value = '';
+        const isbn = document.querySelector('#isbn').value = '';
     }
 }
 //Store class: handles storage (local)
@@ -53,10 +62,23 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
+ 
+    //Validation
+    if(title === '' || author === '' || isbn === '') {
+       alert('Please fill in all the fields');
+    }else {
 
-    //instantiate book 
-    const book = new Book(title, author, isbn);
+        //instantiate book 
+        const book = new Book(title, author, isbn);
+        // Adding new Book to UI, to display on screen after submit
+        UI.addBookToList(book);
+
+        //clear input field
+        UI.clearFields();
+    }
 
 })
 //Event: Remove a Book
- 
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+})
